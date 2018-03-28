93c3712fb8e1836ad112309e49b5f1b33aef27ed
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2018-03-28 18:09:40 +0300

    Participation page

--- app/registration/registration.js
+++ app/registration/registration.js
@@ -1,4 +1,27 @@
+class DashboardController {
+    constructor($http) {
+        this.$http = $http;
+        this.isTermsAndConditions = true;
+        this.isAccepted = false;
+        this.isSignedUp = false;
+    }
+
+    $onInit() {
+    }
+
+    acceptTermsAndConditions() {
+        this.isTermsAndConditions = false;
+        this.isAccepted = true;
+    }
+
+    signUp() {
+        this.isAccepted = false;
+        this.isSignedUp = true;
+    }
+}
+
 angular.module("app")
     .component("registration", {
-        templateUrl: "app/registration/registration.html"
+        templateUrl: "app/registration/registration.html",
+        controller: DashboardController
     });
